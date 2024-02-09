a8bac29807d06f5cf19b4617d265d3c404f9cae8
Ismail Hammounou <ismail.hammounou@allianz.fr>
2024-02-09 02:29:48 +0100

    Delete unnecessary exceptions and unify them

--- src/db2ixf/exceptions.py
+++ src/db2ixf/exceptions.py
@@ -2,166 +2,36 @@
 """Custom exceptions for IXF parsing."""
 
 
+class DataCollectorError(Exception):
+    """Exception raised when facing issues with data collection from the IXF file
+
+    Read the doc: https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types
+    """
+    pass
+
+
 class NotValidColumnDescriptorException(Exception):
     """
     Exception raised when encountering a non valid column descriptor.
 
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-record-types)
+    Read the doc: https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-record-types
     """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
+    pass
 
 
 class UnknownDataTypeException(Exception):
     """
     Exception raised when encountering an unknown data type.
 
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types).
+    Read the doc: https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types
     """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
+    pass
 
 
 class NotValidDataPrecisionException(Exception):
     """
     Exception raised when encountering a non valid data precision.
 
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types).
+    Read the doc https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types
     """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
-
-class ExceedingDefinedMaximumLengthException(Exception):
-    """
-    Exception raised when encountering an object length exceeds the defined
-    maximum length.
-
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types).
-    """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
-
-class LargeObjectLengthException(Exception):
-    """
-    Exception raised when encountering a large object length exceeding the
-    maximum length.
-
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types).
-    """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
-
-class BinaryLengthException(Exception):
-    """
-    Exception raised when encountering binary data type with a length exceeding
-    the maximum length.
-
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types).
-    """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
-
-class CharLengthException(Exception):
-    """
-    Exception raised when encountering char data type with a length exceeding
-    the maximum length.
-
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types).
-    """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
-
-class VarCharLengthException(Exception):
-    """
-    Exception raised when encountering varchar data type with a length
-    exceeding the maximum length.
-
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types).
-    """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
-
-class VarGraphicLengthException(Exception):
-    """
-    Exception raised when encountering VARGRAPHIC data type with a length
-    exceeding the maximum length.
-
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types).
-    """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
-
-class VarGraphicCodePageException(Exception):
-    """
-    Exception raised when encountering VARGRAPHIC data type with DBCS code page,
-    found using IXFCDBCP key, equals to 0.
-
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types).
-    """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
-
-class CLOBCodePageException(Exception):
-    """
-    Exception raised when encountering CLOB data type where SBCP and DBCP are
-    simultaneously equal to 0
-
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types).
-    """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
-
-
-class BlobBinaryStringException(Exception):
-    """Exception raised when encountering a data type different of binary string
-    which occurs when single byte code page equals to 0 but we re not getting
-    a binary string but an other type like character string.
-
-    Read the
-    [doc](https://www.ibm.com/docs/en/db2/11.5?topic=format-pcixf-data-types).
-    """
-
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
+    pass
